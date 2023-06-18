8896b3913cd692e21ac520ee553406a63a8a1535
Shivam <shivamraj7479@gmail.com>
2023-06-18 09:21:36 +0530

    edit the code

--- First.java
+++
@@ -1,8 +0,0 @@
-Class Main
-{
-
-public static void main(String[] args)
-{
-System.out.println("Hello java");
-}
-}
---
+++ FirstString.java
@@ -0,0 +1,13 @@
+package Java;
+
+
+
+//Remoove the spaces between the String:-
+public class FirstString {
+    public static void main(String[] args) {
+        String s=new String("MuzaffarPur Institutte Of Technology");
+        String s2=s.trim();
+        System.out.println(s2);
+    }
+
+}
